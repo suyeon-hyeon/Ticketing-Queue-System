221b48a3866569f4ef462e869ade0242d7c7e42d
suyeon-hyeon <shonee9911@gmail.com>
2026-05-29 00:26:01 +0900

    [✨add] flux 진입 요청 API 개발

--- flow/src/main/java/com/fastcampus/flow/controller/UserQueueController.java
+++ flow/src/main/java/com/fastcampus/flow/controller/UserQueueController.java
@@ -1,12 +1,11 @@
 package com.fastcampus.flow.controller;
 
+import com.fastcampus.flow.dto.AllowUserResponse;
+import com.fastcampus.flow.dto.AllowedUserResponse;
 import com.fastcampus.flow.dto.RegisterUserResponse;
 import com.fastcampus.flow.service.UserQueueService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -20,5 +19,19 @@ public class UserQueueController {
                                                    @RequestParam(name = "user_id") Long userId) {
         return userQueueService.registerWaitQueue(queue, userId)
                 .map(RegisterUserResponse::new);
+    }
+
+    @PostMapping("/allow")
+    public Mono<AllowUserResponse> allowUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                             @RequestParam(name = "count") Long count) {
+        return userQueueService.allowUser(queue, count)
+                .map(allowed -> new AllowUserResponse(count, allowed));
+    }
+
+    @GetMapping("/allowed")
+    public Mono<AllowedUserResponse> isAllowedUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                                   @RequestParam(name = "user_id") Long userId) {
+        return userQueueService.isAllowed(queue, userId)
+                .map(AllowedUserResponse::new);
     }
 }
--- flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
+++ flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
@@ -13,6 +13,7 @@ import java.time.Instant;
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
 
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
         var unixTimestamp = Instant.now().getEpochSecond();
@@ -21,5 +22,17 @@ public class UserQueueService {
                 .switchIfEmpty(Mono.error(ErrorCode.QUEUE_ALREADY_REGISTERED_USER.build()))
                 .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString()))
                 .map(i -> i >= 0 ? i+1: i);
+    }
+
+    public Mono<Long> allowUser(final String queue, final Long count) {
+        return reactiveRedisTemplate.opsForZSet().popMin(USER_QUEUE_WAIT_KEY.formatted(queue), count)
+                .flatMap(member -> reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_PROCEED_KEY.formatted(queue), member.getValue(), Instant.now().getEpochSecond()))
+                .count();
+    }
+
+    public Mono<Boolean> isAllowed(final String queue, final Long userId) {
+        return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_PROCEED_KEY.formatted(queue), userId.toString())
+                .defaultIfEmpty(-1L)
+                .map(rank -> rank >= 0);
     }
 }
