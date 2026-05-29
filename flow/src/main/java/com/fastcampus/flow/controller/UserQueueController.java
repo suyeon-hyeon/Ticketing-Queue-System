12b944c370bd22a5da3af55745b4a0da4f3568c7
suyeon-hyeon <shonee9911@gmail.com>
2026-05-29 23:35:32 +0900

    [✨add] flux 접속 대기 웹페이지 개발

--- flow/src/main/java/com/fastcampus/flow/controller/UserQueueController.java
+++ flow/src/main/java/com/fastcampus/flow/controller/UserQueueController.java
@@ -2,6 +2,7 @@ package com.fastcampus.flow.controller;
 
 import com.fastcampus.flow.dto.AllowUserResponse;
 import com.fastcampus.flow.dto.AllowedUserResponse;
+import com.fastcampus.flow.dto.RankNumberResponse;
 import com.fastcampus.flow.dto.RegisterUserResponse;
 import com.fastcampus.flow.service.UserQueueService;
 import lombok.RequiredArgsConstructor;
@@ -33,5 +34,12 @@ public class UserQueueController {
                                                    @RequestParam(name = "user_id") Long userId) {
         return userQueueService.isAllowed(queue, userId)
                 .map(AllowedUserResponse::new);
+    }
+
+    @GetMapping("/rank")
+    public Mono<RankNumberResponse> getRankUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                                @RequestParam(name = "user_id") Long userId) {
+        return userQueueService.getRank(queue, userId)
+                .map(RankNumberResponse::new);
     }
 }
