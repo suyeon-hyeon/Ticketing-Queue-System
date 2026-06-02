bee11399a63a4eedc5c0acbd6d5f65dd9b3d71fe
suyeon-hyeon <shonee9911@gmail.com>
2026-06-02 23:50:51 +0900

    [🐛fix] flux 대기열 이탈

--- flow/src/test/java/com/fastcampus/flow/service/UserQueueServiceTest.java
+++ flow/src/test/java/com/fastcampus/flow/service/UserQueueServiceTest.java
@@ -137,4 +137,25 @@ public class UserQueueServiceTest {
                 .expectNext(-1L)
                 .verifyComplete();
     }
+
+    @Test
+    void isNotAllowedByToken() {
+        StepVerifier.create(userQueueService.isAllowedByToken("default", 100L, ""))
+                .expectNext(false)
+                .verifyComplete();
+    }
+
+    @Test
+    void isAllowedByToken() {
+        StepVerifier.create(userQueueService.isAllowedByToken("default", 100L, "d333a5d4eb24f3f5cdd767d79b8c01aad3cd73d3537c70dec430455d37afe4b8"))
+                .expectNext(true)
+                .verifyComplete();
+    }
+
+    @Test
+    void generateToken() {
+        StepVerifier.create(userQueueService.generateToken("deafult", 100L))
+                .expectNext("d333a5d4eb24f3f5cdd767d79b8c01aad3cd73d3537c70dec430455d37afe4b8")
+                .verifyComplete();
+    }
 }
